ee2db3475c1573bd26b38b4b165d8ec55a463aeb
Anhdung1478 <Nguyenanhdung1478@gmail.com>
2025-06-04 23:25:35 +0700

    Delete git ignore files and folders

--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj: \
- E:/Project/MarioG/build/_deps/sfml-src/src/SFML/Graphics/BlendMode.cpp \
- E:/Project/MarioG/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
- E:/Project/MarioG/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- E:/Project/MarioG/build/_deps/sfml-src/include/SFML/Config.hpp
--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.obj: \
- E:/Project/MarioG/build/_deps/sfml-src/src/SFML/Graphics/StencilMode.cpp \
- E:/Project/MarioG/build/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
- E:/Project/MarioG/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- E:/Project/MarioG/build/_deps/sfml-src/include/SFML/Config.hpp
