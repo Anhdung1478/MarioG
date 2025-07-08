05ca7b83f3dae6103e633c60832f5130e5a9735d
unknown <minhsunnybear@gmail.com>
2025-07-08 21:29:45 +0700

    create a TextureManager for checkpoint 2

---
+++ src/widget_toolkit/resource/TextureManager.hpp
@@ -0,0 +1,28 @@
+#ifndef TEXTURE_MANAGER_H
+#define TEXTURE_MANAGER_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+struct SpriteData {
+    int x, y;
+    int z, t; // z = width and t = height for sprite
+    const sf::Texture* texture; 
+};
+
+class TextureManager {
+public:
+    void loadSheet(const std::string& jsonPath, const std::string& imagePath);
+
+    const SpriteData& getSpriteData(const std::string& name) const;
+
+private:
+    void loadFromJson(const std::string& jsonPath, sf::Texture& texture);
+
+    std::vector<sf::Texture> textures; 
+    std::unordered_map<std::string, SpriteData> spriteMap; 
+};
+
+#endif
