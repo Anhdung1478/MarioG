b6cb8604d40326128f2f3258bf0c0198af3d032f
Vo Quoc Linh <voquoclinh.29042006@gmail.com>
2025-08-24 13:14:00 +0700

    Add UI for settings page

--- src/main_app/pages/settings.hpp
+++ src/main_app/pages/settings.hpp
@@ -4,15 +4,26 @@
 #include "../../widget_toolkit/controls/slider.hpp"
 #include "../../widget_toolkit/command/key-binding-button.hpp"
 #include "../../widget_toolkit/resource/json.hpp"
+#include "../../widget_toolkit/sfml-extra-feature/rounded-rectangle-shape.hpp"
 #include <memory>
 #include <SFML/Graphics.hpp>
 #include <fstream>
 
 namespace mario::pages {
+    enum class SettingsState {
+        Main,
+        Volume,
+        KeyBindings
+    };
+
     class SettingsPage : public Page {
     private:
+        SettingsState currentState;
         std::unique_ptr<sf::Font> p_font;
         std::unique_ptr<sf::Text> p_title;
+
+        std::unique_ptr<sf::Texture> backgroundTexture;
+        std::unique_ptr<sf::Sprite> backgroundSprite;
 
         std::unique_ptr<sf::Texture> sliderBarTexture;
         std::unique_ptr<sf::Texture> sliderHandleTexture;
@@ -28,6 +39,26 @@ namespace mario::pages {
         std::vector<std::unique_ptr<KeyBindingButton>> keyBindings;
         std::unique_ptr<sf::RectangleShape> confirmButton;
         std::unique_ptr<sf::Text> confirmText;
+
+        bool confirmClicked = false;
+        sf::Clock confirmFlashClock;
+        float confirmAnimDuration = 0.2f;
+
+        bool backClicked = false;
+        sf::Clock backAnimClock;
+        float backAnimDuration = 0.3f;
+
+        // Render textures and sprites for state previews
+        std::unique_ptr<sf::RenderTexture> volumeRenderTexture;
+        std::unique_ptr<sf::Sprite> volumeSprite;
+        std::unique_ptr<sf::RenderTexture> keyBindingsRenderTexture;
+        std::unique_ptr<sf::Sprite> keyBindingsSprite;
+
+        std::unique_ptr<sf::RoundedRectangleShape> leftPanel;
+        std::unique_ptr<sf::RoundedRectangleShape> rightPanel;
+
+        std::unique_ptr<sf::Text> leftPanelTitle;
+        std::unique_ptr<sf::Text> rightPanelTitle;
     public:
         SettingsPage(MainWindow& context);
 
